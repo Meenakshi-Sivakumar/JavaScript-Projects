0832d335306ffb91224fdd1c92c007926fa12336
meenu <meenakshisivakumar08@gmail.com>
2023-04-27 11:14:08 +0530

    2

--- HTML canvas/script.js
+++ HTML canvas/script.js
@@ -3,3 +3,19 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e){
+    if(!isDrawing)return;
+    console.log(e);
+}
+
+canvas.addEventListener('mousemove',draw);
+canvas.addEventListener('mousedown',()=> isDrawing = true);
+canvas.addEventListener('mouseup',()=> isDrawing = false);
+canvas.addEventListener('mouseout',()=> isDrawing = false);
